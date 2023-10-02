d86266c355193cce2ac234abd5846d5dc7d4f3e3
Lev2607 <levent.o@web.de>
2023-10-02 17:03:18 +0200

    initialising ToDo-App with next

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,47 @@
+"use client"
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import button from './/components/button'
+import { useState, useEffect } from "react"
 
-export default function Home() {
+const todos = [
+    { id: 1, task: 'Einkaufen gehen', completed: false },
+    { id: 2, task: 'React-App erstellen', completed: true },
+    { id: 3, task: 'Übungen für JavaScript machen', completed: false },
+  ];
+
+export default function Homepage() {
+  const [myTodos, setMyTodos] = useState(todos)
+
+  const handleAddTodo = () => {
+    setMyTodos(
+      [...myTodos, { id: myTodos.length + 1, task: "Fahrrad reparieren", completed: false }]
+    )
+  }
+
+  useEffect(() => {
+    console.log("my new todos", myTodos)
+  }, [myTodos])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <main>
+        <h1>Welcome to my ToDo Website</h1>
+        <br />
+        <p>Here are my ToDos</p>
+        <br />
+        <ul>
+        { myTodos.map(todo => {
+          return (
+            <li key={todo.id}>{todo.description}{todo.completed}</li>
+          )
+        }) }
+        </ul>
+        <br />
+        <button onClick={handleAddTodo}>Add todo</button>
+      </main>
+    </div>
+    
   )
 }
